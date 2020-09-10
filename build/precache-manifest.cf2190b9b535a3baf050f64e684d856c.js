652f81c3034e556d2e4d56c125973b42e4c054e2
ColorizeMySky <you@example.com>
2020-09-10 01:20:36 +0000

    Yes, I add the build to commit (forgive me, git), cause it was said 'Реализовать сборку проекта'. Actually, npm compile a build by default

---
+++ build/precache-manifest.cf2190b9b535a3baf050f64e684d856c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4679aca288424eb938537a8eb409481a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77efd9339d609a405a3e",
+    "url": "/static/css/main.ae5ead17.chunk.css"
+  },
+  {
+    "revision": "d458b966b8cd19784b8c",
+    "url": "/static/js/2.d2c4aa18.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d2c4aa18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77efd9339d609a405a3e",
+    "url": "/static/js/main.62a5df57.chunk.js"
+  },
+  {
+    "revision": "ba02413f6c97da4cc7cb",
+    "url": "/static/js/runtime-main.b3112dcb.js"
+  }
+]);
